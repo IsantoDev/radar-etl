5ce28829b5020b5d3cb751607425100f4bad4704
IgorBiodev <biovet.santos@gmail.com>
2025-11-28 08:04:54 -0300

    chore: estrutura inicial do projeto e configuracao de ambiente

---
+++ app.py
@@ -0,0 +1,28 @@
+class Buscador:
+    def __init__(self):
+        pass
+
+   
+    def procurar(self, termo):
+        pass
+
+
+class Resumidor:
+    def __init__(self):
+        pass
+
+   
+    def resumir(self, texto):
+        pass
+
+
+class Notificador:
+    def __init__(self):
+        pass
+
+    def enviar(self, relatorio):
+        pass
+
+if __name__ == "__main__":
+    print("Sistema iniciado. Classes definidas.")
+    
