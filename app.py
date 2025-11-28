e01d23f1460e1fc5f1552368474d37e45d85b6ef
IgorBiodev <biovet.santos@gmail.com>
2025-11-28 19:16:19 -0300

    feat: implementa agente resumidor com integracao gemini

--- app.py
+++ app.py
@@ -1,28 +1,73 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+from ddgs import DDGS
+
+
+load_dotenv()
+
 class Buscador:
     def __init__(self):
         pass
 
-   
     def procurar(self, termo):
-        pass
+        with DDGS() as dk:
+            texto = dk.text(termo, max_results=30)
+            return texto
 
 
 class Resumidor:
     def __init__(self):
-        pass
+        print('Iniciando Agente...')
 
-   
+        chave = os.getenv('GEMINI_TOKEN')
+        genai.configure(api_key=chave)
+        self.model = genai.GenerativeModel('gemini-2.0-flash')
+    
+
     def resumir(self, texto):
-        pass
+        text_ia = f'Aqui estão os dados brutos: {texto}'
 
+        prompt = F'''Você é um Engenheiro de IA Sênior. Analise as seguintes notícias coletadas da web:
+        {text_ia}
+
+        Sua missão:
+        1 - Filtre o que é inrrelevante ou incosistente
+        2 - Analise o que é viavel e benefico entre: Noticias,Vagas,Ensinamentos,Tedencias,Educativo
+        3 - Crie um resumo executivo em português (pt-BR)
+        4 - Use topicos (bullet points) e dinamismo para facilitar leitura e absorção do conteudo
+        5 - Priorize novas noticias diarias'''
+
+        print('Agente está pensando...')
+
+        resposta = self.model.generate_content(prompt)
+        return resposta.text
+        
+
+    
 
 class Notificador:
     def __init__(self):
         pass
 
-    def enviar(self, relatorio):
+    def enviar(self,resumo):
+ 
         pass
 
+
 if __name__ == "__main__":
-    print("Sistema iniciado. Classes definidas.")
+    print("Sistema iniciado.")
     
+    scout = Buscador() 
+    analista = Resumidor()
+    dados = scout.procurar("Novas noticias Engenharia de IA")
+
+    relatorio_final = analista.resumir(dados)
+
+    print(relatorio_final)
+
+
+    
+    
+    
+   
